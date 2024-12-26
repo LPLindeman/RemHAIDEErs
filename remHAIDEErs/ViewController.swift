edc237e7aa30b181c80b51aa366a7ba9d14bda57
Peter Lindeman <p.lindeman@me.com>
2024-12-26 19:54:52 +0100

    1.0 first commit

--- remHAIDEErs/ViewController.swift
+++ remHAIDEErs/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startScreenLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dayButtonPressed(_ sender: UIButton) {
+        //print ("Day button pressed 👍")
+       // startScreenLabel.text = "Day button pressed"
+    }
+    
+    @IBAction func weekButtonPressed(_ sender: UIButton) {
+        //print ("Week button pressed ✌️")
+        //startScreenLabel.text = "Week button pressed"
+    }
+    
+    @IBAction func monthButtonPressed(_ sender: UIButton) {
+        //print ("Month button pressed 👌")
+        //startScreenLabel.text = "Month button pressed"
+    }
 }
 
